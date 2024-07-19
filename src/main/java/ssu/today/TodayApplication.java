9a932785256532b49e00fa029151348c617415db
김혜령 <hyeryoung27@soongsil.ac.kr>
2024-07-19 17:01:55 +0900

    [setting] 프로젝트 초기 세팅

---
+++ src/main/java/ssu/today/TodayApplication.java
@@ -0,0 +1,13 @@
+package ssu.today;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodayApplication.class, args);
+	}
+
+}
