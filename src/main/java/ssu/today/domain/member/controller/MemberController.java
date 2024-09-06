8ff6084162cf494e06c2e0d80e3ec2331e056107
김혜령 <hyeryoung27@soongsil.ac.kr>
2024-09-06 15:19:24 +0900

    feat: 특정 회원 정보 조회 api 구현

--- src/main/java/ssu/today/domain/member/controller/MemberController.java
+++ src/main/java/ssu/today/domain/member/controller/MemberController.java
@@ -4,10 +4,12 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ssu.today.domain.member.converter.MemberConverter;
 import ssu.today.domain.member.dto.MemberResponse;
 import ssu.today.domain.member.dto.UserDTO;
 import ssu.today.domain.member.entity.Member;
@@ -25,6 +27,7 @@ import static ssu.today.global.result.code.MemberResultCode.CHECK_MEMBER_REGISTR
 public class MemberController {
 
     private final MemberService memberService;
+    private final MemberConverter memberConverter;
 
     // 닉네임 설정 API
     @PostMapping("/my/nickName")
@@ -39,5 +42,14 @@ public class MemberController {
     @Operation(summary = "현재 유저정보 조회 API", description = "현재 저장된 멤버의 authId로 유저정보를 조회하는 API입니다.")
     public ResultResponse<UserDTO> getMyInfo() {
         return ResultResponse.of(CHECK_MEMBER_REGISTRATION, memberService.getMyInfo());
+    }
+
+    // memberId를 통해 유저정보 조회 API
+    @GetMapping("/{memberId}") // memberId를 사용해 특정 회원 정보 조회
+    @Operation(summary = "특정 회원 정보 조회 API", description = "memberId로 유저 정보를 조회하는 api입니다.")
+    public ResultResponse<MemberResponse.MemberInfo> getMemberInfo(@PathVariable(name = "memberId") Long memberId) {
+        Member member = memberService.findMemberByMemberId(memberId);
+        return ResultResponse.of(MemberResultCode.MEMBER_INFO,
+                memberConverter.toMemberInfo(member));
     }
 }
